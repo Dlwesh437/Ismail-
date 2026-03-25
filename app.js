a58515a1028c227b1fc6c58708e1f6b63906189f
Dlwesh437 <looparr1@gmail.com>
2026-03-25 18:55:38 +0500

    Create app.js with sandwich functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+// Sandwich Cooking Application
+
+function makeSandwich(bread, filling) {
+    return `Your sandwich with ${filling} on ${bread} is ready!`;
+}
+
+const sandwich = makeSandwich('whole grain', 'turkey');
+console.log(sandwich);
